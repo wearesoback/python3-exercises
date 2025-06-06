de13e94cde68c399e6e0da631199aeb22df87ffb
Christian Lopes <backweare@protonmail.com>
2025-06-06 04:47:35 -0400

    basic calculations for ground shipping added

--- Sals-Shipping/shipping.py
+++ Sals-Shipping/shipping.py
@@ -1,1 +1,22 @@
-# new project
+# Sals Shipping project
+
+weight = float(input("What's the weight of your package?: "))
+
+# Ground Shipping
+
+flat_charge = 20.00
+
+if weight <= 2:
+    cost = weight * 1.50
+    print("Your cost is: ", str(cost))
+elif weight > 2 and weight <= 6:
+    cost = (weight * 3.00) + flat_charge
+    print("Your cost is: ", str(cost))
+elif weight > 6 and weight <= 10:
+    cost = (weight * 4.00) + flat_charge
+    print("Your cost is: ", str(cost))
+elif weight > 10:
+    cost = (weight * 4.75) + flat_charge
+    print("Your cost is: ", str(cost))
+else:
+    print ("Are you really trying to ship something?")
