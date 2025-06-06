0ea1bd8034043e3ac9a38b284a3e290f7aa4b90d
Christian Lopes <backweare@protonmail.com>
2025-06-06 04:57:35 -0400

    cheapest shipping method and comments added

--- Sals-Shipping/shipping.py
+++ Sals-Shipping/shipping.py
@@ -5,18 +5,40 @@ weight = float(input("What's the weight of your package?: "))
 # Ground Shipping
 
 flat_charge = 20.00
+cost_ground_premium = 125.00
 
 if weight <= 2:
     cost = weight * 1.50
-    print("Your cost is: ", str(cost))
 elif weight > 2 and weight <= 6:
     cost = (weight * 3.00) + flat_charge
-    print("Your cost is: ", str(cost))
 elif weight > 6 and weight <= 10:
     cost = (weight * 4.00) + flat_charge
-    print("Your cost is: ", str(cost))
 elif weight > 10:
     cost = (weight * 4.75) + flat_charge
-    print("Your cost is: ", str(cost))
 else:
     print ("Are you really trying to ship something?")
+
+# Drone Shipping
+
+if weight <= 2:
+    drone_cost = weight * 4.50
+elif weight > 2 and weight <= 6:
+    drone_cost = (weight * 9.00)
+elif weight > 6 and weight <= 10:
+    drone_cost = (weight * 12.00)
+elif weight > 10:
+    drone_cost = (weight * 14.25) 
+else:
+    print ("Are you really trying to ship something?")
+
+print("Ground Shipping Cost: $", str(cost))
+print("Ground Shipping Premium Cost: $", str(cost_ground_premium))
+print("Drone Shipping Cost: $", str(drone_cost))
+
+# Determine the cheapest shipping method
+if cost < cost_ground_premium and cost < drone_cost:
+    print("\nCheapest Shipping Method: Ground Shipping at $", cost)
+elif cost_ground_premium < cost and cost_ground_premium < drone_cost:
+    print("\nCheapest Shipping Method: Ground Shipping Premium at $", cost_ground_premium)
+else:
+    print("\nCheapest Shipping Method: Drone Shipping at $", drone_cost)
