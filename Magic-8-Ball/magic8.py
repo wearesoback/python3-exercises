622db7d58b5deeaf81783e7db2696fad0ec29519
Christian Lopes <backweare@protonmail.com>
2025-06-06 04:08:07 -0400

    added variables and random number generation for response randomization

--- Magic-8-Ball/magic8.py
+++ Magic-8-Ball/magic8.py
@@ -1,4 +1,10 @@
 # Magic-8-Ball to practice conditional statements, this program will answer "Yes" o "No" based on your fortune :)
+import random
 
-name = "Steve"
+name = input("What's your name?: ")
+question = input("What's your question?: ")
 
+answer = ""
+random_number = random.randint(1,9)
+
+# print(random_number) test out random number generation
