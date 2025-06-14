7de765a5da457069543447ae1cfc2a98efff7fa2
Christian Lopes <backweare@protonmail.com>
2025-06-14 15:40:00 -0400

    fixed call on the Object generation, pd: bro pay attention

--- Basta-Fazoolin/script.py
+++ Basta-Fazoolin/script.py
@@ -26,11 +26,13 @@ class Menu:
 # creating instances for different menus
 
 brunch = Menu("brunch", {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'home fries': 4.50, 'coffee': 1.50, 'espresso': 3.00, 'tea': 1.00, 'mimosa': 10.50, 'orange juice': 3.50},"11am", "4pm" )
-early_bird = Menu("early Bird", "{'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00}", "3pm", "6pm")
-dinner = Menu("dinner", "crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00", "5pm", "11pm")
-kids = Menu("kids", "{'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}", "11am", "9pm")
+early_bird = Menu("early Bird", {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00}, "3pm", "6pm")
+dinner = Menu("dinner", {'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00}, "5pm", "11pm")
+kids = Menu("kids", {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}, "11am", "9pm")
 
 print(brunch)
 
-total_bill = Menu.calculate_bill(brunch, ['pancakes', 'home fries', 'coffee'])
+total_bill = brunch.calculate_bill(['pancakes', 'home fries', 'coffee'])
+total_bill_2 = early_bird.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)'])
 print(total_bill)
+print(total_bill_2)
