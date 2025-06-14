54d308d99b489fdeacb3dff87907f54b09c58ebe
Christian Lopes <backweare@protonmail.com>
2025-06-14 16:04:46 -0400

    class estructure made, project working

--- Basta-Fazoolin/script.py
+++ Basta-Fazoolin/script.py
@@ -21,6 +21,32 @@ class Menu:
 				total += self.items[item]
 
 		return total
+	
+# We are so successful that we need Franchise the entire business 
+
+class Franchise:
+
+	def __init__(self, address, menus):
+		self.address = address
+		self.menus = menus
+   
+	def __repr__(self):
+		return("The restaurant is in {a}".format(a = self.address))
+  
+	def available_menus(self, time):
+		available_menu = []
+		for menu in self.menus:
+			if time >= menu.start_time and time < menu.end_time:
+				available_menu.append(menu)
+		return(available_menu)
+	
+# Let's define our business
+
+class Business:
+
+	def __init__(self, name, franchises):
+		self.name = name
+		self.franchises = franchises
 
 
 # creating instances for different menus
@@ -29,10 +55,17 @@ brunch = Menu("brunch", {'pancakes': 7.50, 'waffles': 9.00, 'burger': 11.00, 'ho
 early_bird = Menu("early Bird", {'salumeria plate': 8.00, 'salad and breadsticks (serves 2, no refills)': 14.00, 'pizza with quattro formaggi': 9.00, 'duck ragu': 17.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 1.50, 'espresso': 3.00}, "3pm", "6pm")
 dinner = Menu("dinner", {'crostini with eggplant caponata': 13.00, 'caesar salad': 16.00, 'pizza with quattro formaggi': 11.00, 'duck ragu': 19.50, 'mushroom ravioli (vegan)': 13.50, 'coffee': 2.00, 'espresso': 3.00}, "5pm", "11pm")
 kids = Menu("kids", {'chicken nuggets': 6.50, 'fusilli with wild mushrooms': 12.00, 'apple juice': 3.00}, "11am", "9pm")
-
-print(brunch)
+arepas_menu = Menu("arepas_menu", {'arepa pabellon': 7.00, 'pernil arepa': 8.50, 'guayanes arepa': 8.00, 'jamon arepa': 7.50}, "10am", "8pm")
+# print(brunch)
 
 total_bill = brunch.calculate_bill(['pancakes', 'home fries', 'coffee'])
 total_bill_2 = early_bird.calculate_bill(['salumeria plate', 'mushroom ravioli (vegan)'])
-print(total_bill)
-print(total_bill_2)
+#print(total_bill)
+#print(total_bill_2)
+
+flagship_store = Franchise("1232 West End Road", [brunch, early_bird, dinner, kids, arepas_menu])
+new_installment = Franchise("12 East Muberry Street", [brunch, early_bird, dinner, kids, arepas_menu])
+arepas_place = Franchise("189 Fitzgerald Avenue", [arepas_menu])
+
+basta = Business("Basta Fazoolin' with my Heart", [flagship_store, new_installment])
+arepa = Business("Take a' Arepa", [arepas_place])
